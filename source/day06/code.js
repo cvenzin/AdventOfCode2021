1a3e1f7250b5b31174f29ce1d0b6beb6f6d71d97
cvenzin <christian.venzin@hexagon.com>
2021-12-06 11:53:00 +0100

    day06

---
+++ source/day06/code.js
@@ -0,0 +1,34 @@
+import {
+    getLines
+} from '../modules/input.js'
+
+const lines = getLines('day06');
+const fish = lines[0].split(',').map(Number);
+
+function part1() {
+    return computeFishCount(80, fish);
+}
+console.log(part1());
+
+function part2() {
+    return computeFishCount(256, fish);
+}
+console.log(part2());
+
+function computeFishCount(days, fish) {
+    // index is timer, value is number of fish
+    const data = new Array(9).fill(0);
+    fish.forEach(f => {
+        data[f]++;
+    });
+
+    for (let i = 0; i < days; i++) {
+        const zeroTimerFish = data[0];
+        // move array to the left and update fish count
+        data.push(zeroTimerFish);
+        data.shift();
+        data[6] += zeroTimerFish;
+    }
+
+    return data.reduce((a, b) => a + b);
+}
